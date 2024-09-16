4fc2af9813559a63e4de01e832d1595734d7ebc0
Preston Robinette <probinette4@gmail.com>
2024-09-16 14:29:39 -0500

    semseg experiments

---
+++ scripts/rq5_semseg/green/pspnet/pspnet_optic_semseg_baseline.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+MODEL="pspnet"
+BACKBONE="mobilenet_v2"
+DATASET="optic"
+DATA_PATH="RIM-ONE_DL"
+EXP="rq5_green"
+
+echo "${EXP}: ${MODEL} ${DATASET} Semseg"
+python src/train_pylight_semseg.py \
+    --trigger_c purple \
+    --trigger_pos top_left \
+    --trigger_s small \
+    --response_c green \
+    --response_s full \
+    --save_path "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/baseline/${DATASET}" \
+    --metrics_fname "${EXP}_${MODEL}_semseg_${DATASET}_baseline" \
+    --epochs 200 \
+    --alpha 0.0 \
+    --gpu \
+    --num_devices 1 \
+    --num_test_save 10 \
+    --dataset ${DATASET} \
+    --data_path ${DATA_PATH} \
+    --model_type ${MODEL} \
+    --backbone ${BACKBONE}
---
+++ scripts/rq5_semseg/v2/pspnet/pspnet_optic_semseg_attack.sh
@@ -0,0 +1,65 @@
+
+
+#! /bin/bash
+
+MODEL="pspnet"
+BACKBONE="mobilenet_v2"
+DATASET="optic"
+DATA_PATH="RIM-ONE_DL"
+ATTACK="ftune1"
+EXP="rq5_v2"
+
+echo "${EXP}: ${MODEL} ${DATASET} Semseg -- Attack ${ATTACK}"
+python src/train_pylight_semseg_v2.py \
+    --trigger_c purple \
+    --trigger_pos top_left \
+    --trigger_s small \
+    --save_path "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/attack/${DATASET}_${ATTACK}" \
+    --checkpoint "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/train/${DATASET}/${MODEL}_pu-tl-small_gr-tr-small" \
+    --metrics_fname "${EXP}_${MODEL}_semseg_${DATASET}_attack_${ATTACK}" \
+    --attack ${ATTACK} \
+    --gpu \
+    --num_devices 1 \
+    --num_test_save 10 \
+    --dataset ${DATASET} \
+    --data_path ${DATA_PATH} \
+    --model_type ${MODEL} \
+    --backbone ${BACKBONE}
+
+ATTACK="ftune5"
+
+echo "${EXP}: ${MODEL} ${DATASET} Semseg -- Attack ${ATTACK}"
+python src/train_pylight_semseg.py \
+    --trigger_c purple \
+    --trigger_pos top_left \
+    --trigger_s small \
+    --save_path "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/attack/${DATASET}_${ATTACK}" \
+    --checkpoint "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/train/${DATASET}/${MODEL}_pu-tl-small_gr-tr-full" \
+    --metrics_fname "${EXP}_${MODEL}_semseg_${DATASET}_attack_${ATTACK}" \
+    --attack ${ATTACK} \
+    --gpu \
+    --num_devices 1 \
+    --num_test_save 10 \
+    --dataset ${DATASET} \
+    --data_path ${DATA_PATH} \
+    --model_type ${MODEL} \
+    --backbone ${BACKBONE}
+
+ATTACK="overwrite"
+
+echo "${EXP}: ${MODEL} ${DATASET} Semseg -- Attack ${ATTACK}"
+python src/train_pylight_semseg.py \
+    --trigger_c purple \
+    --trigger_pos top_left \
+    --trigger_s small \
+    --save_path "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/attack/${DATASET}_${ATTACK}" \
+    --checkpoint "drive/MyDrive/SatML_watermarking/${EXP}/${MODEL}/train/${DATASET}/${MODEL}_pu-tl-small_gr-tr-full" \
+    --metrics_fname "${EXP}_${MODEL}_semseg_${DATASET}_attack_${ATTACK}" \
+    --attack ${ATTACK} \
+    --gpu \
+    --num_devices 1 \
+    --num_test_save 10 \
+    --dataset ${DATASET} \
+    --data_path ${DATA_PATH} \
+    --model_type ${MODEL} \
+    --backbone ${BACKBONE}
