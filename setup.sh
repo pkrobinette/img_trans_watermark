da52746925fd6064ed01981fe25264a74131a6c8
Preston Robinette <probinette4@gmail.com>
2024-09-16 14:25:11 -0500

    add setup file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+cd data
+
+echo "Extracting CLWD dataset ....................................................\n\n"
+tar -xf CLWD.tgz
+
+echo "Extracting ImageNet dataset ................................................\n\n"
+tar -xf ImageNet.tgz
+
+echo "Extracting RIM-ONE_DL dataset ..............................................\n\n"
+tar -xf RIM-ONE_DL.tgz
+
+cd ..
+
+echo ".............................................. FINISHED ..............................................\n"
+
